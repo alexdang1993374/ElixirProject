081cb1008fa3757f961f2dcc4d6fb8f7e72cc38b
unknown <alexdang1993374@gmail.com>
2021-03-02 16:26:07 +0700

    controller can modify database

--- lib/project_web/controllers/registration_controller.ex
+++ lib/project_web/controllers/registration_controller.ex
@@ -8,10 +8,13 @@ defmodule ProjectWeb.RegistrationController do
 
 
 
+
+
   use ProjectWeb, :controller
 
   def register(conn, %{"username" => _, "password" => _} = params) do
-    Project.User.changeset(%Project.User{}, params)
+    alex = Project.User.changeset(%Project.User{}, params)
+    Project.Repo.insert(alex)
     render(conn, "register.json", params: params)
   end
 end
