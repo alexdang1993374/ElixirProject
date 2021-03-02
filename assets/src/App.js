bf525ac44d55a54a045d5d13ea4efde2247bac27
Parintorn Sukhowatanakit <parintorns@gmail.com>
2021-03-02 16:39:26 +0700

    User register

--- assets/src/App.js
+++ assets/src/App.js
@@ -1,11 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+
+const styles = {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    width: "320px",
+  },
+  input: {
+    margin: "5px",
+  },
+};
 
 export default function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function submit() {
+    const url = "http://localhost/4000/register";
+
+    try {
+      const result = await axios({
+        url,
+        method: "post",
+        data: {
+          username,
+          password,
+        },
+      });
+
+      console.log({ result });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
-    <div>
-      <input name="username" />
-      <input name="password" />
-      <button>Login</button>
+    <div style={styles.container}>
+      <input
+        style={styles.input}
+        name="username"
+        onChange={(e) => setUsername(e.target.value)}
+      />
+
+      <input
+        style={styles.input}
+        name="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button style={styles.input} onClick={submit}>
+        Login
+      </button>
     </div>
   );
 }
